5be796fba06c9dd75b41b6f55588fa44e02f799f
joelhoppe <103591780+joelhoppe@users.noreply.github.com>
2022-04-13 11:29:19 -0500

    Update client_main.cpp

--- client_main.cpp
+++ client_main.cpp
@@ -1,6 +1,6 @@
 // client_main.cpp
 //   This function serves as the "main" function for the client-side
-#include "TicTacToe.h"
+#include "NIM.h"
 #include <iostream>
 #include <string>
 #include <WinSock2.h>
@@ -13,19 +13,19 @@ int client_main(int argc, char* argv[], std::string playerName)
 	char protocol[4] = "udp";
 	char* _host = NULL;
 
-	SOCKET s = connectsock(_host, TicTacToe_UDPPORT, protocol);		// Create a socket  (host and port not used here for UDP)
+	SOCKET s = connectsock(_host, NIM_UDPPORT, protocol);		// Create a socket  (host and port not used here for UDP)
 
-	// Find all TicTacToe servers
-	std::cout << std::endl << "Looking for TicTacToe servers ... " << std::endl;
+	// Find all NIM servers
+	std::cout << std::endl << "Looking for NIM servers ... " << std::endl;
 	int numServers;
 	getServers(s, GetBroadcastAddress(), TicTacToe_UDPPORT, server, numServers);
 
 	if (numServers == 0) {
-		std::cout << std::endl << "Sorry.  No TicTacToe servers were found.  Try again later." << std::endl << std::endl;
+		std::cout << std::endl << "Sorry.  No NIM servers were found.  Try again later." << std::endl << std::endl;
 	}
 	else {
 		// Display the list of servers found
-		std::cout << std::endl << "Found TicTacToe server";
+		std::cout << std::endl << "Found NIM server";
 		if (numServers == 1) {
 			std::cout << ":" << "  " << server[0].name << std::endl;
 		}
@@ -60,14 +60,14 @@ int client_main(int argc, char* argv[], std::string playerName)
 			host = server[answer - 1].host;
 			port = server[answer - 1].port;
 
-			// Append playerName to the TicTacToe_CHALLENGE string & send a challenge to host:port
+			// Append playerName to the NIM_CHALLENGE string & send a challenge to host:port
 			char buf[MAX_SEND_BUF];
-			strcpy_s(buf, TicTacToe_CHALLENGE);
+			strcpy_s(buf, NIM_CHALLENGE);
 			strcat_s(buf, playerName.c_str());
 			int len = UDP_send(s, buf, strlen(buf) + 1, (char*)host.c_str(), (char*)port.c_str());
 
 			// Play the game.  You are the 'X' player
-			int winner = playTicTacToe(s, serverName, host, port, X_PLAYER);
+			int winner = playNIM(s, serverName, host, port, X_PLAYER);
 		}
 	}
 
