465ed1b2aa48f76927d3711a3d077206ca8b1f6c
Nestor Alfaro <nestoralfaro@Nestors-MacBook-Pro.local>
2022-04-15 00:31:02 -0500

    First commit from vscode

--- playNIM.cpp
+++ playNIM.cpp
@@ -1,7 +1,6 @@
 // playNIM.cpp
 // This set of functions are used to actually play the game.
 // Play starts with the function: playNIM() which is defined below
-
 #include "NIM.h"
 #include <WinSock2.h>
 #include <iostream>
